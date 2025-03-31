e563c25e08afe0a4d71c19e7b011f7297c8af1e3
Klopprock <philippschroeder1986@web.de>
2025-03-31 15:40:42 +0200

    5Tulpen/des/comment 0.2

--- Skript/Tulpa-List.js
+++ Skript/Tulpa-List.js
@@ -12,7 +12,7 @@ const Tulpas = {
     // ein weisses kleines, süsses , simples Kaninchen
     "Hase":{name:"Hase",HP:90,HP_Total:90, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"-"},des:"Ein Hase, der gerne mit seinen Freunden spielt. Manchmal schubst er auch alte Hasenrentner.. aber nur weil sie lustig fallen.",},
     //etwas grösser auch mit grimmigen gesicht und längeren Ohren
-    "Kaninchen des Todes":{name:"Kaninchen des Todes",HP:100,HP_Total:100, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"Todeshauch"}, des: "Der eine Hase sie alle zu knechten... und... zu töten!",},
+    "Kaninchen_des_Todes":{name:"Kaninchen des Todes",HP:100,HP_Total:100, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"Todeshauch"}, des: "Der eine Hase sie alle zu knechten... und... zu töten!",},
     //Im grunde darf es auch nur schlicht und weiss sein. Vielleicht mir feueroten augen
     
     
@@ -24,17 +24,17 @@ const Tulpas = {
     //Ein Schäferhund mit Rüstung um den Hals
     
    
-    "Kleiner Wolf":{name:"Kleiner Wolf",HP:80,HP_Total:80, attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des: "Ein junger kleiner Wolf. Eigentlich ist er ganz nett, aber auch etwas wild.",}, 
+    "Kleiner_Wolf":{name:"Kleiner Wolf",HP:80,HP_Total:80, attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des: "Ein junger kleiner Wolf. Eigentlich ist er ganz nett, aber auch etwas wild.",}, 
     //ein kleiner grau/weisser Wolf
     "Wolf":{name:"Wolf",HP:90,HP_Total:90, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"},des: "Ein Klassiker unter den Tulpas. Der den Wolf nicht ehrt... ist des Schafes nicht wert."}, 
     //Etwas grösser . Ein voll ausgewchsener Wolf
-    "Böser Wolf":{name:"Böser Wolf",HP:110,HP_Total:110, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Todeshauch"},des: "Der gute alte Böse Wolf. Wenn er grade keine Grossmütter tötet, dann ärgert er kleine Kinder"}, 
+    "Böser_Wolf":{name:"Böser Wolf",HP:110,HP_Total:110, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Todeshauch"},des: "Der gute alte Böse Wolf. Wenn er grade keine Grossmütter tötet, dann ärgert er kleine Kinder"}, 
     //Dieser Wolf ist Pechschwarz und hat rote Augen. Er sieht sehr böse aus.
     
     
     "Zosse":{name:"Zosse",HP:90,HP_Total:90, attacks:{1:"Tackle", 2:"Hufschlag", 3:"-", 4:"-"},des: "Ein etwas klappriger Gaul. Nicht sicher ob er noch lange macht."}, 
     //ein klappriges standart/Braunes Pferd
-    "Treues Ross":{name:"Treues Ross",HP:100,HP_Total:100, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"-"},des: "Doch nicht so klapprig wie gedacht. Ein treues Ross, das immer für dich da ist."}, 
+    "Treues_Ross":{name:"Treues Ross",HP:100,HP_Total:100, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"-"},des: "Doch nicht so klapprig wie gedacht. Ein treues Ross, das immer für dich da ist."}, 
     //Dasselbe nur ohne klapprig :)
     "Streitross":{name:"Streitross",HP:110,HP_Total:110, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"Ausweichen"},des: "Der Panzer unter den Pferden. Stolz, Kräftig und mutig. Ein besseres Pferd hat es nie gegeben."},
     //Dieses Pferd ist in Pferderüstung eingepackt. Klassisches Ritterpferd.
