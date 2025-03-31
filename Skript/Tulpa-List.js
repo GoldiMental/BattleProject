42b977ee1c3cb966966c5ce640e5767bb47943bf
Klopprock <philippschroeder1986@web.de>
2025-03-31 12:59:30 +0200

    Update Tulpa-List.js

--- Skript/Tulpa-List.js
+++ Skript/Tulpa-List.js
@@ -1,3 +1,6 @@
 const Tulpas = {
-    "Mustername":{name:"Mustername",HP:100,HP_Total:100,}
+    "Mustername":{name:"Mustername",HP:100,HP_Total:100,}, 
+    "Mustername":{name:"Mustername",HP:100,HP_Total:100,}, 
+    "Mustername":{name:"Mustername",HP:100,HP_Total:100,}, 
+    "Mustername":{name:"Mustername",HP:100,HP_Total:100,}, 
 } 
