b4846d24e220e6ecb3ff40fe6638491d5e21b1ee
Klopprock <philippschroeder1986@web.de>
2025-04-02 14:50:10 +0200

    tulpas0.3

--- Skript/Tulpa-List.js
+++ Skript/Tulpa-List.js
@@ -1,42 +1,42 @@
 const Tulpas = {
    
-    Mäusschen:{name:"Mäusschen",HP:60,HP_Total:60,ANG:6,VER:6, attacks:{1:"Kratzer", 2:"Biss", 3:"-", 4:"-"}, des:"Ein Süßer und netter Zeitgenosse. Immer Gut gelaunt und voller energie geht das Mäusschen seiner Täglichen Mäusearbeit nach. Ärgere sie doch einfach nicht.",}, 
+    Mäusschen:{name:"Mäusschen",className: "Mäusschen", HP:60,HP_Total:60,ANG:6,VER:6, attacks:{1:"Kratzer", 2:"Biss", 3:"-", 4:"-"}, des:"Ein Süßer und netter Zeitgenosse. Immer Gut gelaunt und voller energie geht das Mäusschen seiner Täglichen Mäusearbeit nach. Ärgere sie doch einfach nicht.",}, 
     //ein simple braun/goldene kleine maus
-    Maus:{name:"Maus",HP:80,HP_Total:80,ANG:7,VER:7, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"}, des: "Eine sehr Gesellige Maus. Trotzdem mag sie kleine Auseinadnersetzungen, sie siehts sportlich.",}, 
+    Maus:{name:"Maus",className: "Maus",HP:80,HP_Total:80,ANG:7,VER:7, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"}, des: "Eine sehr Gesellige Maus. Trotzdem mag sie kleine Auseinadnersetzungen, sie siehts sportlich.",}, 
     // änlich wie die erste , nur etwas grösser mit ernstem  gesicht
-    Kampfmaus:{name:"Kampfmaus",HP:100,HP_Total:100,ANG:8,VER:8, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Schlag"}, des: "Ein wahrer Mäusesoldat. Sie ist sehr mutig und hat keine Angst vor größeren Gegnern.",}, 
+    Kampfmaus:{name:"Kampfmaus",className: "Kampfmaus",HP:100,HP_Total:100,ANG:8,VER:8, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Schlag"}, des: "Ein wahrer Mäusesoldat. Sie ist sehr mutig und hat keine Angst vor größeren Gegnern.",}, 
     // diese maus sieht auch gleich aus wie die vorherige, allerdings hat sie ein rotes stirnband
     
     
-    Kaninchen:{name:"Kaninchen",HP:70,HP_Total:70,ANG:6,VER:6,attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des:"Ein süßes und nettes Kaninchen. Es ist sehr schüchtern und hat Angst vor... allem! ",}, 
+    Kaninchen:{name:"Kaninchen",className: "Kaninchen",HP:70,HP_Total:70,ANG:6,VER:6,attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des:"Ein süßes und nettes Kaninchen. Es ist sehr schüchtern und hat Angst vor... allem! ",}, 
     // ein weisses kleines, süsses , simples Kaninchen
-    Hase:{name:"Hase",HP:90,HP_Total:90,ANG:7,VER:7, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"-"},des:"Ein Hase, der gerne mit seinen Freunden spielt. Manchmal schubst er auch alte Hasenrentner.. aber nur weil sie lustig fallen.",},
+    Hase:{name:"Hase",className: "Hase",HP:90,HP_Total:90,ANG:7,VER:7, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"-"},des:"Ein Hase, der gerne mit seinen Freunden spielt. Manchmal schubst er auch alte Hasenrentner.. aber nur weil sie lustig fallen.",},
     //etwas grösser auch mit grimmigen gesicht und längeren Ohren
-    Kaninchen_des_Todes:{name:"Kaninchen des Todes",HP:100,HP_Total:100,ANG:10,VER:10, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"Todeshauch"}, des: "Der eine Hase sie alle zu knechten... und... zu töten!",},
+    Kaninchen_des_Todes:{name:"Kaninchen des Todes",className: "Kaninchen_des_Todes",HP:100,HP_Total:100,ANG:10,VER:10, attacks:{1:"Tackle", 2:"Biss", 3:"Ausweichen", 4:"Todeshauch"}, des: "Der eine Hase sie alle zu knechten... und... zu töten!",},
     //Im grunde darf es auch nur schlicht und weiss sein. Vielleicht mir feueroten augen
     
     
-    Streuner:{name:"Streuner",HP:80,HP_Total:80,ANG:7,VER:7, attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des: "Ein Streuner, der immer auf der Suche nach einem neuen Zuhause ist. Es sieht aus als würde er bei dir bleiben wollen.",}, 
+    Streuner:{name:"Streuner",className: "Streuner",HP:80,HP_Total:80,ANG:7,VER:7, attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des: "Ein Streuner, der immer auf der Suche nach einem neuen Zuhause ist. Es sieht aus als würde er bei dir bleiben wollen.",}, 
     //Ein netter brauner Hund/Mischling aus Schäferhund und Doberman.
-    Wachhund:{name:"Wachhund",HP:90,HP_Total:90,ANG:8,VER:8, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"},des: "Ein Wachhund, der immer auf der Hut ist. Er ist sehr treu und greift das an was sein Herrchen bedroht.",}, 
+    Wachhund:{name:"Wachhund",className: "Wachhund",HP:90,HP_Total:90,ANG:8,VER:8, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"},des: "Ein Wachhund, der immer auf der Hut ist. Er ist sehr treu und greift das an was sein Herrchen bedroht.",}, 
     //Ein Doberman mit Nietenhalsband
-    Schutzhund:{name:"Schutzhund",HP:105,HP_Total:105,ANG:9,VER:9, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Schild"},des: "Ein Engel... vom Spagettimonster geschickt um sein Herrchen zu schützen. Freut sich wenn er lob bekommt."}, 
+    Schutzhund:{name:"Schutzhund",className: "Schutzhund",HP:105,HP_Total:105,ANG:9,VER:9, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Schild"},des: "Ein Engel... vom Spagettimonster geschickt um sein Herrchen zu schützen. Freut sich wenn er lob bekommt."}, 
     //Ein Schäferhund mit Rüstung um den Hals
     
    
-    Kleiner_Wolf:{name:"Kleiner Wolf",HP:80,HP_Total:80,ANG:8,VER:6, attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des: "Ein junger kleiner Wolf. Eigentlich ist er ganz nett, aber auch etwas wild.",}, 
+    Kleiner_Wolf:{name:"Kleiner Wolf",className: "Kleiner_Wolf",HP:80,HP_Total:80,ANG:8,VER:6, attacks:{1:"Tackle", 2:"Biss", 3:"-", 4:"-"},des: "Ein junger kleiner Wolf. Eigentlich ist er ganz nett, aber auch etwas wild.",}, 
     //ein kleiner grau/weisser Wolf
-    Wolf:{name:"Wolf",HP:90,HP_Total:90,ANG:9,VER:8, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"},des: "Ein Klassiker unter den Tulpas. Der den Wolf nicht ehrt... ist des Schafes nicht wert."}, 
+    Wolf:{name:"Wolf",className: "Wolf",HP:90,HP_Total:90,ANG:9,VER:8, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"-"},des: "Ein Klassiker unter den Tulpas. Der den Wolf nicht ehrt... ist des Schafes nicht wert."}, 
     //Etwas grösser . Ein voll ausgewchsener Wolf
-    Böser_Wolf:{name:"Böser Wolf",HP:110,HP_Total:110,ANG:10,VER:9, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Todeshauch"},des: "Der gute alte Böse Wolf. Wenn er grade keine Grossmütter tötet, dann ärgert er kleine Kinder"}, 
+    Böser_Wolf:{name:"Böser Wolf",className: "Böser_Wolf",HP:110,HP_Total:110,ANG:10,VER:9, attacks:{1:"Tackle", 2:"Biss", 3:"Blutlust", 4:"Todeshauch"},des: "Der gute alte Böse Wolf. Wenn er grade keine Grossmütter tötet, dann ärgert er kleine Kinder"}, 
     //Dieser Wolf ist Pechschwarz und hat rote Augen. Er sieht sehr böse aus.
     
     
-    Zosse:{name:"Zosse",HP:90,HP_Total:90,ANG:6,VER:8, attacks:{1:"Tackle", 2:"Hufschlag", 3:"-", 4:"-"},des: "Ein etwas klappriger Gaul. Nicht sicher ob er noch lange macht."}, 
+    Zosse:{name:"Zosse",className: "Zosse",HP:90,HP_Total:90,ANG:6,VER:8, attacks:{1:"Tackle", 2:"Hufschlag", 3:"-", 4:"-"},des: "Ein etwas klappriger Gaul. Nicht sicher ob er noch lange macht."}, 
     //ein klappriges standart/Braunes Pferd
-    Treues_Ross:{name:"Treues Ross",HP:100,HP_Total:100,ANG:7,VER:9, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"-"},des: "Doch nicht so klapprig wie gedacht. Ein treues Ross, das immer für dich da ist."}, 
+    Treues_Ross:{name:"Treues Ross",className: "Treues_Ross",HP:100,HP_Total:100,ANG:7,VER:9, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"-"},des: "Doch nicht so klapprig wie gedacht. Ein treues Ross, das immer für dich da ist."}, 
     //Dasselbe nur ohne klapprig :)
-    Streitross:{name:"Streitross",HP:110,HP_Total:110,ANG:8,VER:10, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"Ausweichen"},des: "Der Panzer unter den Pferden. Stolz, Kräftig und mutig. Ein besseres Pferd hat es nie gegeben."},
+    Streitross:{name:"Streitross",className: "Streitross",HP:110,HP_Total:110,ANG:8,VER:10, attacks:{1:"Tackle", 2:"Hufschlag", 3:"Schild", 4:"Ausweichen"},des: "Der Panzer unter den Pferden. Stolz, Kräftig und mutig. Ein besseres Pferd hat es nie gegeben."},
     //Dieses Pferd ist in Pferderüstung eingepackt. Klassisches Ritterpferd.
     
 
