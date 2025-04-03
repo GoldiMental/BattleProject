81be7f947396b99754a457f84412f8a86f2a55ce
JohnnyTec <johnny.hoffmann@tn.techstarter.de>
2025-04-03 12:10:50 +0200

    Update Player.js

--- Skript/Player.js
+++ Skript/Player.js
@@ -32,12 +32,12 @@ else {
         Gold: 500,
         Cheats: 0,
         Tulpas: {
-            Slot_1: { name: "", Lv: 0, XP: 0, ID: 0 },
-            Slot_2: { name: "", Lv: 0, XP: 0, ID: 0 },
-            Slot_3: { name: "", Lv: 0, XP: 0, ID: 0 },
-            Slot_4: { name: "", Lv: 0, XP: 0, ID: 0 },
-            Slot_5: { name: "", Lv: 0, XP: 0, ID: 0 },
-            Slot_6: { name: "", Lv: 0, XP: 0, ID: 0 },
+            Slot_1: { name: "", Lv: 0, HP: 0, XP: 0, ID: 0 },
+            Slot_2: { name: "", Lv: 0, HP: 0, XP: 0, ID: 0 },
+            Slot_3: { name: "", Lv: 0, HP: 0, XP: 0, ID: 0 },
+            Slot_4: { name: "", Lv: 0, HP: 0, XP: 0, ID: 0 },
+            Slot_5: { name: "", Lv: 0, HP: 0, XP: 0, ID: 0 },
+            Slot_6: { name: "", Lv: 0, HP: 0, XP: 0, ID: 0 },
             PC: [],
         },
         inventory: {
