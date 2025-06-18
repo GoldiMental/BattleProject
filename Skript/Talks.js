7fb22c849cdbf95a5b73e99185f8337cf912a815
JohnnyTec <johnny.hoffmann@tn.techstarter.de>
2025-06-18 10:25:40 +0200

    Update Talk

--- Skript/Talks.js
+++ Skript/Talks.js
@@ -1,5 +1,8 @@
 function ProfTalk_1(){
     console.log('Gespräch 1 mit Prof');
+    //Hier startet der Monolog
+
+    // Ändert den Monolog für den nächsten Klick
     document.getElementById("Professor").innerHTML = '<button id="ProfessorButton" onclick="ProfTalk_2()"> </button>';
 }
 
