f18086effafd36a44256eba59533adc133413a20
JohnnyTec <johnny.hoffmann@tn.techstarter.de>
2025-06-23 15:24:51 +0200

    Shop Step 1
    - shows BuyList and close it

--- Skript/Shop.js
+++ Skript/Shop.js
@@ -1,9 +1,13 @@
 const Shops = {
-  Shop_1: ["Tulpaball","Heiltrank"]
+  Shop_1: ["Tulpaball", "Heiltrank"],
+  Shop_2: ["Tulpaball", "Heiltrank", "Super_Tulpaball"],
 }
 
-async function shopHandel() {
+let activeShop = "";
+
+async function shopHandel(SHOP) {
   clearInterval(moveIntervalID);
+  activeShop = SHOP;
   let monologBox = document.getElementsByClassName("TrainerDialogBox")[0];
   let shopMenu = document.getElementById("shopMenu");
   monologBox.style.visibility = "visible";
@@ -21,3 +25,26 @@ function closeShop() {
   moveIntervalID = setInterval(() => { if (activeDirection) { moveMap() }; }, moveInterval);
 }
 
+function buyItems() {
+  let ShopBuyList = document.getElementById("ShopBuyList");
+  let html = "";
+  ShopBuyList.style.visibility = "visible";
+  for (itm in Shops[activeShop]) {
+    html += '<div style="height:50px;"><div class="producttitle">' + Item_List[Shops[activeShop][itm]].name + '</div>' +
+      '<input id="qty_'+itm+'" class="number_input" type="number" min="1" onchange="calculate(this.value ,\'product_' + itm + '\' ,' + Item_List[Shops[activeShop][itm]].price + ')" value="1">' +
+      '<div class="productcost" id="product_' + itm + '">' + Item_List[Shops[activeShop][itm]].price + ' Gold</div>' +
+      '<button class="BuyButton" onclick="BuyThis('+itm+')">Buy</button></div>';
+  }
+  html += '<button onclick="close_BuyList()">Exit</button>';
+  ShopBuyList.innerHTML = html;
+}
+
+function calculate(qty, Product, Price) {
+  console.log(qty, Product, Price)
+  let output = document.getElementById(Product);
+  output.innerHTML = qty * Price + " Gold";
+}
+
+function close_BuyList(){
+  document.getElementById("ShopBuyList").style.visibility="hidden";
+}
