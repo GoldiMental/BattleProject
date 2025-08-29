dcf7fad33a1956341b23c268431f83805bda65df
Jaymental <djohnnyhoffmann@gmail.com>
2025-08-29 15:54:39 +0200

    Balancing BaseDMG

--- Skript/attack.js
+++ Skript/attack.js
@@ -1,11 +1,11 @@
 const Attacks = {
-    "Tackle": { name: "Tackle", ATK_Power: 20, DEF_Value: 0 },
-    "Kratzer": { name: "Kratzer", ATK_Power: 21, DEF_Value: 0 },
-    "Hufschlag": { name: "Hufschlag", ATK_Power: 30, DEF_Value: 0 },
-    "Biss": { name: "Biss", ATK_Power: 35, DEF_Value: 0 },
-    "Blutlust": { name: "Blutdurst", ATK_Power: 40, DEF_Value: 0 },
-    "Schlag": { name: "Schlag", ATK_Power: 45, DEF_Value: 0 },
-    "Todeshauch": { name: "Todeshauch", ATK_Power: 50, DEF_Value: 0 },
+    "Tackle": { name: "Tackle", ATK_Power: 10, DEF_Value: 0 },
+    "Kratzer": { name: "Kratzer", ATK_Power: 11, DEF_Value: 0 },
+    "Hufschlag": { name: "Hufschlag", ATK_Power: 15, DEF_Value: 0 },
+    "Biss": { name: "Biss", ATK_Power: 17, DEF_Value: 0 },
+    "Blutlust": { name: "Blutdurst", ATK_Power: 25, DEF_Value: 0 },
+    "Schlag": { name: "Schlag", ATK_Power: 27, DEF_Value: 0 },
+    "Todeshauch": { name: "Todeshauch", ATK_Power: 35, DEF_Value: 0 },
     "Schild": { name: "Schild", ATK_Power: 0, DEF_Value: 20 },
     "Ausweichen": { name: "Ausweichen", ATK_Power: 0, DEF_Value: 0 },
 }
