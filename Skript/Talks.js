c3dfe94c3ed140041fefb870d1d8174d6b35f036
Jaymental <djohnnyhoffmann@gmail.com>
2025-09-01 21:38:04 +0200

    Update Talks.js

--- Skript/Talks.js
+++ Skript/Talks.js
@@ -7,9 +7,9 @@ function ProfTalk_2(){
     console.log('Gespräch 2 mit Prof');
 }
 
-function starteTutorial(){
+async function starteTutorial(){
     console.log("Tutorial wird gestartet...");
-    showCustomAlert("Herzlich Willkommen Trainer ",Player.name);
-    showCustomAlert('Ich hoffe Du bist gut vorbereitet. Wenn nicht, sieh Dir gerne mal die "TulpaKing Info & Impressum"-Seite an.');
-    showCustomAlert('Für Probleme wende dich bitte an: goldimental@gmx.de');
+    await showCustomConfirm("Herzlich Willkommen Trainer ",Player.name);
+    await showCustomConfirm('Ich hoffe Du bist gut vorbereitet. Wenn nicht, sieh Dir gerne mal die "TulpaKing Info & Impressum"-Seite an.');
+    await showCustomConfirm('Für Probleme wende dich bitte an: goldimental@gmx.de');
 }
