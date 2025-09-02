68df4a0b0fc196b4465711d880b2e384b566cd44
Jaymental <djohnnyhoffmann@gmail.com>
2025-09-02 11:19:56 +0200

    Create ColorSettings.js

---
+++ Skript/ColorSettings.js
@@ -0,0 +1,5 @@
+function setcolor() {
+    if (firmcolor != undefined) {
+        document.getElementById('stil').setAttribute('href', 'Styles/Style_DarkGalaxyHome-' + firmcolor + '.css');
+    }
+}
