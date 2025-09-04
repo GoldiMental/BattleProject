2a95526fa34907abf095b11294efc37266c2252b
Jaymental <djohnnyhoffmann@gmail.com>
2025-09-04 02:51:50 +0200

    Update from Jaymental Version: 0.28

--- Skript/Talks.js
+++ Skript/Talks.js
@@ -8,10 +8,10 @@ function ProfTalk_2() {
 }
 
 async function starteTutorial() {
-    console.log("Tutorial wird gestartet..."); await Delay(200);
-    await showCustomConfirm("Herzlich Willkommen Trainer " + Player.name);
-    await showCustomConfirm('Ich hoffe Du bist gut vorbereitet. Wenn nicht, sieh Dir gerne mal die "TulpaKing Info & Impressum"-Seite an.');
-    await showCustomConfirm('Für Probleme wende dich bitte an: goldimental@gmx.de');
-    await showCustomConfirm('Vergiss nicht regelmäßig zu speichern! 😉\n\nViel Spaß beim Spielen');
+    console.log("Tutorial wird gestartet..."); await Delay(100);
+    await showCustomConfirm("Herzlich Willkommen Trainer " + Player.name); await Delay(100);
+    await showCustomConfirm('Ich hoffe Du bist gut vorbereitet. Wenn nicht, sieh Dir gerne mal die "TulpaKing Info & Impressum"-Seite an.'); await Delay(100);
+    await showCustomConfirm('Für Probleme wende dich bitte an: goldimental@gmx.de'); await Delay(100);
+    await showCustomConfirm('Vergiss nicht regelmäßig zu speichern! 😉\n\nViel Spaß beim Spielen'); await Delay(100);
     movementGame.classList.toggle("hidethis", false);
 }
