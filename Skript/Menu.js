c4b732cb4d372a9ffe70dd4047fe232949afa895
Jaymental <djohnnyhoffmann@gmail.com>
2025-09-05 16:03:19 +0200

    Update from Jaymental Version: 0.110

--- Skript/Menu.js
+++ Skript/Menu.js
@@ -235,7 +235,8 @@ async function Use(itm, qty) {
             close_Items(); Tulpas_List();
             const buttonsArray = [];
             for (i = 0; i <= 6; i++) {
-                if (Player.Tulpas["Slot_" + i].name != "") {
+                let slot = "Slot_" + i;
+                if (Player.Tulpas[slot].name != "") {
                     let obj = { text: Player.Tulpas["Slot_" + i].name, value: i };
                     buttonsArray.push(obj);
                 }
