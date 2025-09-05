ff4eaa7f28583641675e68b344e1d3e6f28e3825
Jaymental <djohnnyhoffmann@gmail.com>
2025-09-05 13:08:44 +0200

    Update from Jaymental Version: 0.89

--- Skript/battle.js
+++ Skript/battle.js
@@ -549,8 +549,8 @@ async function UseBall(ball) {
             console.log("UseBall(", ball, ") ✅ => Gefangen => Leite Spieler zurück auf Karte...");
         }
         else {
-            document.getElementById('tulpaball').style.backgroundImage = "url(BattleProject/img/textures/tBall_single)"; await Delay(2000);
-            document.getElementById('tulpaball').style = "";
+            document.getElementById('tulpaball').classList.toggle("tulpaballGIF", true); await Delay(2000);
+            document.getElementById('tulpaball').classList.toggle("tulpaballGIF", false);
             document.getElementById('Tulpa-opp').style.opacity = "1";
             document.getElementById('tulpaball').parentNode.removeChild(document.getElementById('tulpaball'));
             document.getElementById('battle_text').innerText = "Mist, es hat sich befreit"; await Delay(2000);
